1275741de7a79669cbdb8cc39b2cb3dbef3dca94
C. Huhn <spoved@knollhuhn.de>
2014-11-23 22:09:05 +0100

    Add logging

--- recipes/weather.rb
+++ recipes/weather.rb
@@ -5,4 +5,6 @@
 woeid = 654200
 interval = 3600
 
-fhem_command "define Weather1 Weather #{woeid} #{interval or ''}"
+fhem_command "define Weather_yahoo Weather #{woeid} #{interval or ''}"
+fhem_command "define FL_Weather_yahoo FileLog ./log/weather-%Y-%m.log Weather_yahoo"
+fhem_command "attr FL_Weather_yahoo logtype text"
